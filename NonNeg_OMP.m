f5909e4eccd8e754b565f7a3a35968714a64b101
GSDrenthen <40467047+GSDrenthen@users.noreply.github.com>
2018-11-17 21:08:55 +0100

    Update NonNeg_OMP.m

--- NonNeg_OMP.m
+++ NonNeg_OMP.m
@@ -3,6 +3,12 @@ function [MWF] = NonNeg_OMP(A,y,T2Times,RI)
 % Matching Pursuit to better suit the problem of Myelin Water Fraction
 % estimation.
 %
+% Please cite the following paper when using this function:
+% Drenthen GS, Backes WH, Aldenkamp AP, Op ’t Veld GJ, Jansen JFA. A new
+% analysis approach for T2 relaxometry myelin water quantification: 
+% Orthogonal Matching Pursuit. Magn Reson Med. 2018;00:1–12. 
+% https://doi.org/10.1002/mrm.27600
+%
 % Original implementation by Mehrad Yaghoobi (http://www.mehrdadya.com/)
 % Adaptations by Gerhard Drenthen
 
